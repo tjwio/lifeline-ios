34a0d7a49167bde1eb4475e208f819dd565891db
Tim Wong <tim@tjw.io>
2019-02-16 12:54:40 -0800

    initial commit

---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'lifeline' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for lifeline
+  pod 'ArcGIS-Runtime-SDK-iOS', '100.4'
+  pod 'SnapKit', '~> 4.0.0'
+
+  target 'lifelineTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'lifelineUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ lifeline/HomeViewController.swift
@@ -0,0 +1,32 @@
+//
+//  HomeViewController.swift
+//  lifeline
+//
+//  Created by Tim Wong on 2/16/19.
+//  Copyright © 2019 tjwio. All rights reserved.
+//
+
+import UIKit
+import ArcGIS
+import SnapKit
+
+class HomeViewController: UIViewController {
+    
+    let mapView: AGSMapView = {
+        let mapView = AGSMapView()
+        mapView.map = AGSMap(basemap: .streetsNightVector())
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return mapView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(mapView)
+        
+        mapView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+}
